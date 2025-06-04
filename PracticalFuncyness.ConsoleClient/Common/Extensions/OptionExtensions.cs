60621f0b0db029756216e44be866ec3ea32538ce
Paul Kent <paulandrewkent@hotmail.com>
2025-06-04 08:14:02 +0100

    Corrected typo.

--- PracticalFuncyness.ConsoleClient/Common/Extensions/OptionExtensions.cs
+++ PracticalFuncyness.ConsoleClient/Common/Extensions/OptionExtensions.cs
@@ -5,20 +5,24 @@ namespace PracticalFuncyness.ConsoleClient.Common.Extensions;
 public static class OptionExtensions
 {
 
-    public static Option<TOut> AndThen<TIn, TOut>(this Option<TIn> Option, Func<TIn, Option<TOut>> onValue) where TIn : notnull where TOut : notnull
+    public static Option<TOut> AndThen<TIn, TOut>(this Option<TIn> option, Func<TIn, Option<TOut>> onValue) where TIn : notnull where TOut : notnull
 
-        => Option.Bind(onValue);
+        => option.Bind(onValue);
 
-    public static Option<TOut> AndThen<TIn, TOut>(this Option<TIn> Option, Func<TIn, TOut> onValue) where TIn : notnull where TOut : notnull
+    public static Option<TOut> AndThen<TIn, TOut>(this Option<TIn> option, Func<TIn, TOut> onValue) where TIn : notnull where TOut : notnull
 
-        => Option.Map(onValue);
+        => option.Map(onValue);
 
 
-    public static Option<T> OrElse<T>(this Option<T> Option, Func<Option<T>> onNone) where T : notnull
+    public static Option<T> OrElse<T>(this Option<T> option, Func<Option<T>> onNone) where T : notnull
 
-        => Option.IsSome ? Option : onNone();
+        => option.IsSome ? option : onNone();
 
-    public static Option<T> OrElse<T>(this Option<T> Option, Func<T> onNone) where T : notnull
+    public static Option<T> OrElse<T>(this Option<T> option, Func<T> onNone) where T : notnull
 
-        => Option.IsSome ? Option : onNone();//uses implicit conversion so null will be none.
+        => option.IsSome ? option : onNone();//uses implicit conversion so null will be none.
+
+
+
+
 }
